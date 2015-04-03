d8cdb95ec5c436ff57b025a1abf3a52f4849acff
SammyK <sammyk@sammykmedia.com>
2015-04-03 10:39:33 -0500

    Add session prefix for CSRF token in OAuth 2.0 flow

--- src/LaravelFacebookSdk/LaravelFacebookRedirectLoginHelper.php
+++ src/LaravelFacebookSdk/LaravelFacebookRedirectLoginHelper.php
@@ -4,13 +4,18 @@ use Facebook\FacebookRedirectLoginHelper;
 
 class LaravelFacebookRedirectLoginHelper extends FacebookRedirectLoginHelper
 {
+    /**
+     * @const string Prefix to use for session variables.
+     */
+    const SESSION_PREFIX = 'FBRLH_';
+
     protected function storeState($state)
     {
-        \Session::put('state', $state);
+        \Session::put(static::SESSION_PREFIX . 'state', $state);
     }
 
     protected function loadState()
     {
-        return $this->state = \Session::get('state');
+        return $this->state = \Session::get(static::SESSION_PREFIX . 'state');
     }
 }
